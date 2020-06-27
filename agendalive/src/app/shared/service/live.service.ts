c8f38a0a4e9bc3c388e87def63e6d031b1cd43c0
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-06-27 19:28:41 -0300

    Chamando a API

--- agendalive/src/app/shared/service/live.service.ts
+++ agendalive/src/app/shared/service/live.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Live } from '../model/live.model';
 import { ResponsePageable } from '../model/responsePageable.model';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class LiveService {
 
   apiUrl = 'http://localhost:8080/lives';
@@ -19,7 +18,15 @@ export class LiveService {
     private httpClient: HttpClient
   ) { }
 
-  public getLivesWithFlas(flag: string): Observable<ResponsePageable> {
-    return this.httpClient.get<ResponsePageable>(this.apiUrl + '?flag=' + flag)
+  public getLives(): Observable<ResponsePageable> {
+    return this.httpClient.get<ResponsePageable>(this.apiUrl);
+  }
+
+  public getLivesWithFlag(flag: string): Observable<ResponsePageable> {
+    return this.httpClient.get<ResponsePageable>(this.apiUrl + '?flag=' + flag);
+  }
+
+  public postLives(live: any): Observable<Live> {
+    return this.httpClient.post<any>(this.apiUrl, live, this.httpOptions);
   }
 }
--- agendalive/src/app/view/home/live-list/live-list.component.ts
+++ agendalive/src/app/view/home/live-list/live-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LiveService } from 'src/app/shared/service/live.service';
+import { Live } from 'src/app/shared/model/live.model';
 
 @Component({
   selector: 'app-live-list',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiveListComponent implements OnInit {
 
-  constructor() { }
+  livesPrevious: Live[];
 
-  ngOnInit() {
+  constructor(
+    public liveService: LiveService
+  ) { }
+
+  ngOnInit(): void {
+    this.getLives();
+  }
+
+  getLives() {
+    this.liveService.getLivesWithFlag('previous').subscribe(
+      data => {
+        this.livesPrevious = data.content;
+        console.log(this.livesPrevious);
+      }
+    );
   }
 
 }
