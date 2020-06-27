69f08fa236e54f91c369fcf397266c2aa02ea259
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-06-27 18:45:12 -0300

    Ajustando API

--- backend/src/main/java/com/spring/agendalive/AgendaliveApplication.java
+++ backend/src/main/java/com/spring/agendalive/AgendaliveApplication.java
@@ -1,9 +1,14 @@
 package com.spring.agendalive;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.spring.agendalive.document"})
 public class AgendaliveApplication {
 
 	public static void main(String[] args) {
