0334fa976ed77211fdf082b304e736099de91f0e
Wagner <wg.o.costa@gmail.com>
2020-07-14 22:20:24 -0300

    Alterando o projeto para frontend para padronizar.

---
+++ frontend/src/app/views/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { LiveFormDialogComponent } from './live-form-dialog/live-form-dialog.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(
+    public dialog: MatDialog
+  ) { }
+
+  ngOnInit() {
+  }
+
+  addLive(): void {
+    const dialogRef = this.dialog.open(LiveFormDialogComponent, {
+      maxHeight: '95vh',
+      minWidth: '400px',
+      width: '25vw',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+}
